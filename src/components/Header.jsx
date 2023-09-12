bf523bdf5bf78a10980aa0fa7d56f644826610e9
FelipeZambelloSv <rm99843@fiap.com.br>
2023-09-12 09:58:04 -0300

    Adicionando importações do header

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../assets/Logo.svg';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import styles from './Header.module.css';
