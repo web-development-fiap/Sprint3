c9c6cff40b9afa61dc37d3d7ec9d764a02b4aedf
elencabral1 <rm98790@fiap.com.br>
2023-09-12 13:01:14 -0300

    começando a importar

---
+++ src/components/Home.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { ReactComponent as Bike } from '../assets/Bike.svg';
+import { ReactComponent as Vistoria } from '../assets/VistoriaImg.svg';
+import { ReactComponent as Nota } from '../assets/Nota.svg';
