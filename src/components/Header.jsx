49d515f7b30a8a5aaf68c689e65b9e90a8a0646e
LuigiUchiyama <luigi_uchiyama@outlook.com>
2023-09-12 09:02:46 -0300

    arquivo Header e css

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../assets/Logo.svg';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={`container ${styles.header}`}>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <ul className={styles.menu}>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>|</li>
+        <li>
+          <Link to="/vistoria">Vistoria</Link>
+        </li>
+        <li>|</li>
+        <li>
+          <Link to="/equipe">Equipe</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
