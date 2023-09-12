17ec2bbccb402062a9f78f555fece4bb7b7a22c4
elencabral1 <rm98790@fiap.com.br>
2023-09-12 13:05:48 -0300

    começando a criar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,3 +9,20 @@ import Img from '../assets/Group.png';
 import '../App.css';
 import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+      <div className="container2">
+        <div className={styles.home}>
+          <div className={styles.bike}>
+            <h1>O seu seguro do seu jeito.</h1>
+            <p>
+              Em parceria com a empresa MELFE SOLUTIONS, nós da porto seguro
+              oferecemos um serviço dinâmico e ágil para sua bicicleta.
+            </p>
+            <Link to="/vistoria">
+              <button className="button">Solicitar seguro</button>
+            </Link>
+          </div>
+          <Bike />
+        </div>
