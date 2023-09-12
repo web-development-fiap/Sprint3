e83b856c07fdf6c2c6ad4b50238dbc90af202f4c
FelipeZambelloSv <rm99843@fiap.com.br>
2023-09-12 09:58:43 -0300

    Finalizando o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,3 +3,28 @@ import { ReactComponent as Logo } from '../assets/Logo.svg';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+      <div className={`container ${styles.header}`}>
+        <Link to="/">
+          <Logo />
+        </Link>
+        <ul className={styles.menu}>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>|</li>
+          <li>
+            <Link to="/vistoria">Vistoria</Link>
+          </li>
+          <li>|</li>
+          <li>
+            <Link to="/equipe">Equipe</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Header;
