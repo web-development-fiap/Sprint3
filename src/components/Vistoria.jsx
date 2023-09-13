de0713fa65cab1cf24b253f204f346cec337dd2b
crushedbyamallet <RM550242@fiap.com.br>
2023-09-12 22:04:17 -0300

    imports vist

---
+++ src/components/Vistoria.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ReactComponent as Imagem } from '../assets/imagem.svg';
+import { ReactComponent as Camera } from '../assets/Camera.svg';
+import { ReactComponent as VistoriaImg } from '../assets/Arquivo.svg';
+import styles from './Vistoria.module.css';
+import '../App.css';
+
