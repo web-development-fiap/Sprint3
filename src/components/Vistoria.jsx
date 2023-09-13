01f91729edb04ce7b065811d787560be920dde60
crushedbyamallet <RM550242@fiap.com.br>
2023-09-12 22:08:09 -0300

    const

--- src/components/Vistoria.jsx
+++ src/components/Vistoria.jsx
@@ -5,3 +5,10 @@ import { ReactComponent as VistoriaImg } from '../assets/Arquivo.svg';
 import styles from './Vistoria.module.css';
 import '../App.css';
 
+const Vistoria = () => {
+    return (
+      <div className={`container ${styles.vistoria}`}>
+        <h1>Vistoria Porto seguro BIKE</h1>
+        <p>Preencha os Formularios com atenção.</p>
+        <div className={styles.opcoes}>
+          <h1>Selecione uma opção</h1>
