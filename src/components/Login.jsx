f24c1c7d0e87630ddf0ead47d60401c749e90479
luanaduque <luanaduque273@gmail.com>
2023-09-12 21:08:18 -0300

    add email e senha

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,10 @@
 import React, { useState } from 'react';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(`Email: ${email}, Senha: ${password}`);
+    };
